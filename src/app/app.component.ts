520e18f3bef8dbd565340acf53da285c6452e40c
Patrick <patrickmahinay031@gmail.com>
2023-02-06 13:39:13 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+// import { Apollo, gql } from 'apollo-angular';
+
+// const loginUser = gql` 
+//     query LoginUser($username: String!, $password: String!) {
+//         loginUser(username: $username, password: $password) {
+//         message,
+//         status
+//     }
+// }`;
+
+interface Credential {
+  username: String;
+  password: String;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.pug',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todolist';
+  // constructor(private apollo: Apollo) { }
+
+  // Types  
+  Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+
+
+  // Variables
+  userCredential: Credential = {
+    username: "",
+    password: ""
+  }
+  title: string = 'todolist';
+  todoObject: any = [];
+  todoName: string = "";
+  todoList: string[] = [
+    'Washing',
+    'Biking',
+    'Working',
+    'Teaching',
+    'Running'
+  ]
+
+  // Actions
+  addNewTodo = (data: any): any | void => this.todoList.push(data);
+  handleOnchange = (event: Event | string | any) => {
+    const { target: { value, name } } = event ?? {};
+    const { userCredential } = this || {};
+    let propHolder: any = {};
+    propHolder = { ...userCredential, [name]: value };
+    return this.userCredential = propHolder;
+  }
+
+
+  onSubmit = (event: Event | string | any): any => {
+    event.preventDefault();
+
+    const { username, password } = this.userCredential || {};
+    console.log(`this.userCredential`, this.userCredential)
+
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular'
+import { HttpLink } from 'apollo-angular/http'
+import { InMemoryCache } from '@apollo/client/core'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ApolloModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory(httpLink: HttpLink) {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'https://48p1r2roz4.sse.codesandbox.io'
+          })
+        }
+      },
+      deps: [HttpLink]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
